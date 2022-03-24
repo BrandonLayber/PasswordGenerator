8b0558d96e7ca2ea7a3696963dd1905a49166659
unknown <brandon.layber@yahoo.com>
2022-03-23 23:26:27 -0500

    added conditional to password selector

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,14 @@
 // Assignment code here
 var characterLength = 8;
 var choiceArr = [];
-var lowerCaseArr = ["a", "b", "c" ,"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t" ,"u", "v", "w", "x", "y", "z"];  
-var upperCaseArr = ["A", "B", "C" ,"D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T" ,"U", "V", "W", "X", "Y", "Z"];  
+var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];  
+var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];  
 var numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];  
-var specialCharacterArr = ["!", "@", "#" ,"$", "%", "^", "&", "*", "(", ")", "<", ">", "?", "*",];  
+var specialCharacterArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '?', '*'];  
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -18,15 +16,22 @@ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var correctPrompts = getPrompts();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  
+  if (correctPrompts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  }
 }
 
 function generatePassword() {
-
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
+  }
+  return password;
 }
 
 function getPrompts() {
