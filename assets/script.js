ae721bb1724c834912d2fb3c524e14fdad4e7885
unknown <brandon.layber@yahoo.com>
2022-03-25 17:15:30 -0500

    updates

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// Assignment code here
+// Array code here
 var characterLength = 8;
 var choiceArr = [];
 var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];  
